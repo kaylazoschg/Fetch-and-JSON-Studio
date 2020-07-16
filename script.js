5a4757eb461f874079d5defbd5528bae6e534156
Kayla Zoschg <kaylazoschg@Kaylas-MacBook-Pro-2.local>
2020-07-15 21:16:04 -0400

    finished studio

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 // TODO: add code here
+window.addEventListener("load", function() {
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then( function(response) {
+       response.json().then( function(json) {
+          const div = document.getElementById("container");
+          // Add HTML that includes the JSON data
+          for (let i=0; i<json.length; i++){
+          div.innerHTML += `
+        <div class="astronaut">
+            <div class="bio">
+                <h3>${json[i].firstName} ${json[i].lastName}</h3>  
+                <ul>
+                    <li>Temp ${json[i].hoursInSpace}</li>
+                    <li>Wind Speed ${json[i].active}</li>
+                    <li>Status ${json[i].skills}</li>
+                </ul>
+                </div>
+            <img class="avatar" src="${json[i].picture}">
+        </div>
+          `
+          }
+       });
+    });
+ });
